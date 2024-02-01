1c6b7857ecc1df321671d98a85079bcb36466302
shashirith <iec2020069@iiita.ac.in>
2024-02-01 17:01:49 +0530

    fix:chatpage skeliton

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -2,11 +2,17 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>Next Chat</title>
       </Head>
-      <h1>Welcome to the Chat</h1>
-    </div>
+      <div className="grid h-screen grid-cols-[260px_1fr]">
+        <div>sidebar</div>
+        <div className="flex flex-col bg-slate-600">
+          <div className="flex-1">chat Window</div>
+          <footer className="bg-slate-800 p-10">Footer</footer>
+        </div>
+      </div>
+    </>
   );
 }
