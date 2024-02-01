24aab9d14d26bdbcebdaf0bef9c491ae71901457
shashirith <iec2020069@iiita.ac.in>
2024-02-01 16:13:11 +0530

    feat:Added Authentication Logic

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import Head from "next/head";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
-    <div>
+    <>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>Chatty-witty</title>
       </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
-    </div>
+      <div>
+        {user ? (
+          <Link href="/api/auth/logout">Logout</Link>
+        ) : (
+          <Link href="/api/auth/login">Login</Link>
+        )}
+      </div>
+    </>
   );
 }
