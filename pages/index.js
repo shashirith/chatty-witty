187318166ebf0991604ae119cf218ce959b0e032
shashirith <iec2020069@iiita.ac.in>
2024-02-01 16:39:31 +0530

    fix:signup-homepage

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,39 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
+
+  const AuthLink = () => {
+    // Determines the href and text based on the user's authentication status
+    const href = user ? "/api/auth/logout" : "/api/auth/login";
+    const text = user ? "Logout" : "Login";
+
+    return (
+      <Link
+        href={href}
+        className="rounded-md bg-emerald-500 px-4 py-2 font-bold text-white hover:bg-emerald-600"
+      >
+        {text}
+      </Link>
+    );
+  };
 
   return (
     <>
       <Head>
         <title>Chatty-witty</title>
       </Head>
-      <div>
-        {user ? (
-          <Link href="/api/auth/logout">Logout</Link>
-        ) : (
-          <Link href="/api/auth/login">Login</Link>
+      <div className="flex min-h-screen items-center justify-center bg-slate-800 text-center text-white">
+        <AuthLink />
+        {!user && (
+          <Link
+            href="/api/auth/signup"
+            className="ml-4 rounded-md bg-emerald-500 px-4 py-2 font-bold text-white hover:bg-emerald-600"
+          >
+            SignUp
+          </Link>
         )}
       </div>
     </>
